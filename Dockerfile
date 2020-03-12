c98e055b43410ce5dfb1b1fbe487d24dcc33ad6b
chatlamin <viktorap@inbox.ru>
2020-03-12 13:29:38 +0300

    work

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ubuntu:18.04
+
+RUN apt-get -qq update \
+  && apt-get install -qq --yes --no-install-recommends sendemail \
+     libio-socket-ssl-perl \
+     libnet-ssleay-perl \
+  && apt-get -qq clean \
+  && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
+  && sed -i "1933s/.*/if (\! IO::Socket::SSL->start_SSL(\$SERVER, SSL_version => \'SSLv23:\!SSLv2\', SSL_verify_mode => 0)) {/" /usr/bin/sendEmail
+
+CMD ["/bin/bash"]
---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##Mail
+MAIL_FROM='отправитель@gmail.com'
+MAIL_TO='адресат@gmail.com'
+MAIL_NAME='backup mysql'
+MAIL_BODY="ERROR"
+MAIL_SMTP='smtp.gmail.com:587'
+MAIL_LOGIN='login@gmail.com'
+MAIL_PASS='password'
+
+docker run --rm \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /etc/timezone:/etc/timezone:ro \
+    chatlamin/sendemail-docker:latest \
+    sendemail \
+    -o tls=yes \
+    -f $MAIL_FROM \
+    -t $MAIL_TO \
+    -s $MAIL_SMTP \
+    -xu $MAIL_LOGIN \
+    -xp $MAIL_PASS \
+    -u "$MAIL_NAME" \
+    -m "$MAIL_BODY"
